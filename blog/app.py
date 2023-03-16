a31bb8f981174c57465613c368e7f4039b48146c
lawyer2021 <gconsul@mail.com>
2023-03-16 17:10:43 +0300

    Including Flask-SQLAlchemy in Flask project; creating base model of user; adding authorization page, creating view, not permission anonymous.

--- blog/app.py
+++ blog/app.py
@@ -3,9 +3,19 @@ from flask import Flask, request, g, render_template
 from werkzeug.exceptions import BadRequest
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models import User
+from blog.views.auth import login_manager, auth_app
+from blog.models.database import db
+
 app = Flask(__name__)
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+app.config["SECRET_KEY"] = "abcdefg123456"
+app.register_blueprint(auth_app, url_prefix="/auth")
+login_manager.init_app(app)
 
 @app.route("/")
 def index():
@@ -81,5 +91,32 @@ def handle_zero_division_error(error):
     print(error)  # prints str version of error: 'division by zero'
     app.logger.exception("Here's traceback for zero division error")
     return "Never divide by zero!", 400
+
+
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
 
 
